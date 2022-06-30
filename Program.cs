da91fc90e57a57a0af0c754eb76c29a2d98425ec
Kalid-N <107488332+Mckali-2k@users.noreply.github.com>
2022-06-30 15:41:01 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Lab_1st_Assignment
+{
+    class Program
+    {
+        // The conosle App is guess the number
+        static void Main(string[] args)
+        { 
+
+            int n;
+            char again;
+
+            do
+            {
+                Console.WriteLine("Enter you choice (0 to 10)");
+                n = int.Parse(Console.ReadLine());
+
+                checkNum(n);
+
+                Console.WriteLine("\nDo You want to try again(y/n)?");
+                again = char.Parse(Console.ReadLine());
+
+                Console.Clear();
+
+            } while (again == 'y' || again == 'Y');
+
+        }
+
+        public static int  randomNum()
+        {
+            Random rnd = new Random();
+
+            return rnd.Next(10);
+        }
+
+        public static void checkNum(int n)
+        {
+            int randNum = randomNum();
+
+            if (n == randNum)
+            {
+                Console.WriteLine($"You Won, The Number is {randNum}");
+            }
+            else Console.WriteLine($"You Lost, The Number Was {randNum}");
+        }
+    }
+}
